d6a41c44d5067e0201b16dca60f20c7e7c399b61
Miesvanderlippe <miesvanderlippe@gmail.com>
2017-01-30 17:58:57 +0100

    Linefollower sensor

--- IPOMEDT/classes/lineFollower.py
+++ IPOMEDT/classes/lineFollower.py
@@ -1,3 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+
+
 class LineFollower:
-    def __init__(self):
-        pass
+
+    def __init__(self, pin):
+        self.pin = pin
+        GPIO.setup(self.pin, GPIO.IN)
+
+    def poll(self):
+        return GPIO.input(self.pin) == 0
+
+
+def main() -> None:
+    GPIO.cleanup()
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    sensor = LineFollower(25)
+
+    for i in range(0, 100):
+        print(sensor.poll())
+        time.sleep(0.1)
+
+if __name__ == '__main__':
+    main()
