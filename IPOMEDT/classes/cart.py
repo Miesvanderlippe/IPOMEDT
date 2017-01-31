9e0e65e55d45fb63191cdcae7123b68cfafc5438
Miesvanderlippe <miesvanderlippe@gmail.com>
2017-01-31 12:04:41 +0100

    added senor,last turn dir&partial turns

--- IPOMEDT/classes/cart.py
+++ IPOMEDT/classes/cart.py
@@ -3,8 +3,10 @@ import time
 
 if __name__ == "__main__":
     from motor import Motor
+    from ultraSonic import UltraSonic
 else:
     from classes.motor import Motor
+    from classes.ultraSonic import UltraSonic
 
 
 class Cart:
@@ -12,8 +14,11 @@ class Cart:
     def __init__(self):
         self.l_wheel = Motor([9, 10])
         self.r_wheel = Motor([7, 8])
+        self.ultrasonic = UltraSonic([17, 18])
+        self.prev_turn = 'left'
 
     def turn_right(self, speed, ratio):
+        self.prev_turn = 'right'
         if ratio > 0:
             self.l_wheel.forward(speed * ratio)
             self.r_wheel.forward(speed)
@@ -22,6 +27,7 @@ class Cart:
             self.r_wheel.forward(speed)
 
     def turn_left(self, speed, ratio):
+        self.prev_turn = 'left'
         if ratio > 0:
             self.r_wheel.forward(speed * ratio)
             self.l_wheel.forward(speed)
@@ -29,7 +35,15 @@ class Cart:
             self.r_wheel.backward(speed * (ratio * -1))
             self.l_wheel.forward(speed)
 
-    def stop(self):
+    def turn_left_tick(self, ticks=1, speed=15, ratio=-1):
+        self.turn_left(speed, ratio)
+        time.sleep(0.1 * ticks)
+
+    def turn_right_tick(self, ticks=1, speed=21, ratio=-1):
+        self.turn_right(speed, ratio)
+        time.sleep(0.06 * ticks)
+
+    def stop(selgf):
         self.l_wheel.stop()
         self.r_wheel.stop()
 
