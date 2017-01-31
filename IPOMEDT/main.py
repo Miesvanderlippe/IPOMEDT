6073d661d13fc4299bdeb8106f709fc34234d4aa
glodinas <mail@cihatkaya.nl>
2017-01-31 20:55:24 +0100

    Test 1

--- IPOMEDT/main.py
+++ IPOMEDT/main.py
@@ -1,6 +1,7 @@
 from classes.motor import Motor
 from classes.lineFollower import LineFollower
 from classes.ultraSonic import UltraSonic
+import time
 
 
 def main() -> None:
@@ -9,7 +10,22 @@ def main() -> None:
     linefollower = LineFollower(25)
     ultrasonic = UltraSonic([17, 18])
 
-    raise NotImplementedError()
+    while True:
+        time.sleep(0.5)
+        testmotor1.forward()
+        if ultrasonic.poll() == 10:
+            time.sleep(0.5)
+            testmotor1.backward()
+            testmotor2.backward()
+        else:
+            for i in range(0, 50):
+                testmotor2.forward()
+                time.sleep(0.012)
+
+                testmotor2.backward()
+                time.sleep(0.012)
+
+                testmotor2.stop()
 
 if __name__ == "__main__":
     main()
