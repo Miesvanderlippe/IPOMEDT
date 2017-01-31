a1a7ce80ef28a97e54ec3b640bdc112b51eabc71
vinzce <rc_dom@protonmail.ch>
2017-01-31 21:42:46 +0100

    multiply

--- IPOMEDT/classes/carClass.py
+++ IPOMEDT/classes/carClass.py
@@ -8,9 +8,9 @@ class CarClass:
         self.motor2 = Motor([8, 7])
 
     def turnLeft(self, speed, ratio):
-        self.motor1.forward(speed + ratio)
-        self.motor2.backward(speed + ratio)
+        self.motor1.forward(speed * ratio)
+        self.motor2.backward(speed * ratio)
 
     def turnRight(self, speed, ratio):
-        self.motor1.backward(speed + ratio)
-        self.motor2.forward(speed + ratio)
+        self.motor1.backward(speed * ratio)
+        self.motor2.forward(speed * ratio)
