533e964ff8279c34ad09a42334a5895c91124275
vinzce <rc_dom@protonmail.ch>
2017-01-31 12:15:12 +0100

    lights import, try except

--- IPOMEDT/navigation.py
+++ IPOMEDT/navigation.py
@@ -1,3 +1,4 @@
+from classes import light
 from classes import motor
 from classes import lineFollower
 
@@ -10,19 +11,29 @@ def main() -> None:
     motor1 = Motor([10, 9])
     moter2 = Moter([8, 7])
 
+    light = Light()
+
     lineFollow = LineFollower()
 
-    while True:
-        sleep(0.1)
-        if lineFollow == True :
-            print("Blue line")
-            motor1.forward()
-            motor2.forward()
-            sleep(0.5)
-        else:
-            print("not blue line")
-            motor1.stop()
-            motor2.stop()
+    try:
+        while True:
+            light.turn_on()
+            sleep(0.1)
+            if lineFollow == True :
+                print("Blue line")
+                motor1.forward()
+                motor2.forward()
+                sleep(0.2)
+            else:
+                print("not blue line")
+                motor1.stop()
+                motor2.stop()
+                sleep(0.2)
+                #kijk naar rechts
+                #kijk naar links
+
+    except KeyboardInterrupt:
+        GPIO.cleanup()
 
 if __name__ == __main__ :
     main()
