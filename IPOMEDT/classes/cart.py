ee2f56498362827fe706b56811cf58fdfe2b0b58
Miesvanderlippe <miesvanderlippe@gmail.com>
2017-01-31 13:09:54 +0100

    Flashing siren leds

--- IPOMEDT/classes/cart.py
+++ IPOMEDT/classes/cart.py
@@ -1,12 +1,15 @@
 import RPi.GPIO as GPIO
 import time
+from threading import Thread
 
 if __name__ == "__main__":
     from motor import Motor
     from ultraSonic import UltraSonic
+    from light import Light
 else:
     from classes.motor import Motor
     from classes.ultraSonic import UltraSonic
+    from classes.light import Light
 
 
 class Cart:
@@ -14,8 +17,13 @@ class Cart:
     def __init__(self):
         self.l_wheel = Motor([9, 10])
         self.r_wheel = Motor([7, 8])
+        self.l_light = Light(21)
+        self.r_light = Light(20)
         self.ultrasonic = UltraSonic([17, 18])
         self.prev_turn = 'left'
+
+        thread = Thread(target=self.siren_loop)
+        thread.start()
 
     def turn_right(self, speed, ratio):
         self.prev_turn = 'right'
@@ -55,6 +63,28 @@ class Cart:
         self.r_wheel.backward(speed)
         self.l_wheel.backward(speed)
 
+    def siren_loop(self):
+        while True:
+            self.l_light.dim(100)
+            self.r_light.dim(100)
+            time.sleep(0.05)
+
+            self.l_light.dim(0)
+            self.r_light.dim(0)
+            time.sleep(0.05)
+
+            self.r_light.dim(100)
+            time.sleep(0.05)
+
+            self.r_light.dim(0)
+            time.sleep(0.05)
+
+            self.l_light.dim(100)
+            time.sleep(0.05)
+
+            self.l_light.dim(0)
+            time.sleep(0.05)
+
 
 def main() -> None:
 
@@ -63,19 +93,10 @@ def main() -> None:
 
     cart = Cart()
 
-    cart.forward(20)
-    time.sleep(0.7)
-
-    cart.backward(20)
-    time.sleep(0.7)
-
-    cart.turn_left(20, 0.1)
-    time.sleep(0.9)
-
-    cart.turn_right(20, 0.1)
-    time.sleep(0.9)
-
-    cart.stop()
+    time.sleep(10)
+    print("Twerkt dit?")
+    time.sleep(10)
+    print("'t werkt")
 
 if __name__ == "__main__":
     main()
