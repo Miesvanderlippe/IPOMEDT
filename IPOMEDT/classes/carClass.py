660a4b3149a3da9d19f0238a44b8a15f9d9600f6
vinzce <rc_dom@protonmail.ch>
2017-01-31 18:32:09 +0100

    carClass and navigation fixes

---
+++ IPOMEDT/classes/carClass.py
@@ -0,0 +1,11 @@
+from classes.motor import Motor
+
+class CarClass:
+
+    def __init__(self):
+        self.motor1 = Motor([10, 9])
+        self.motor2 = Motor([8, 7])
+
+    def turnLeft(self, speed, ratio):
+        self.motor1.forward(speed + ratio)
+        self.motor2.backward(speed + ratio)
--- IPOMEDT/navigation.py
+++ IPOMEDT/navigation.py
@@ -1,65 +1,72 @@
 import time
-from classes import light
-from classes import motor
-from classes import lineFollower
+from classes.light import Light
+from classes.motor import Motor
+from classes.carClass import CarClass
+import RPi.GPIO as GPIO
+from classes.lineFollower import LineFollower
 
 def main() -> None:
 
-    GPIO.cleanup()
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
 
     motor1 = Motor([10, 9])
-    moter2 = Moter([8, 7])
+    motor2 = Motor([8, 7])
 
-    light = Light()
+    light = Light(21)
 
-    lineFollow = LineFollower()
+    lineFollow = LineFollower(25)
+    carClass = CarClass()
 
     direction_time =  30
 
     try:
         while True:
             light.turn_on()
-            sleep(0.1)
-            if lineFollow == True :
+            time.sleep(0.1)
+            if lineFollow.poll() == False:
                 print("Blue line")
-                motor1.forward(30)
-                motor2.forward(30)
-                sleep(0.2)
+                motor1.backward(5)
+                motor2.backward(5)
+                time.sleep(0.2)
             else:
-                print("not blue line")
+                print("not blue line " + str(lineFollow.poll()))
                 motor1.stop()
                 motor2.stop()
-                sleep(0.2)
+                time.sleep(0.5)
 
-                while lineFollow == False :
-                    if direction_time <= 30 :
+                while lineFollow.poll() == True:
+                    time.sleep(0.5)
+                    if direction_time <= 30:
                         #kijk rechts
-                        print("kijk naar rechts")
+                        print("kijk naar rechts " + str(lineFollow.poll()))
+                        #motor1.forward(15)
+                        #motor2.stop()
                         direction_time =- 1
 
                     #timer terug naar 30
                     direction_time = 30
 
-                    if direction_time <= 30 :
+                    if direction_time <= 30:
                         #kijk links
-                        print("kijk naar links")
+                        print("kijk naar links " + str(lineFollow.poll()))
+                        carClass.turnLeft(15, 5)
                         direction_time =- 1
 
                     #timer terug naar 30
                     direction_time = 30
 
-                    if direction_time <= 30 :
+                    if direction_time <= 30:
                         #kijk vooruit
-                        print("kijk vooruit")
+                        print("kijk vooruit " + str(lineFollow.poll()))
+                        #motor1.backward(15)
+                        #motor2.backward(15)
                         direction_time =- 1
 
                     #timer terug naar 30
                     direction_time = 30
-
     except KeyboardInterrupt:
         GPIO.cleanup()
 
-if __name__ == __main__ :
+if __name__ == '__main__':
     main()
