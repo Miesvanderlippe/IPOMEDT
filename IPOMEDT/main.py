e29710db7dc9a2df48e0c9c20e89304fef5cc69f
Cihat Kaya <mail@cihatkaya.nl>
2017-02-01 21:24:57 +0100

    Cancel Scritp verwerkt

--- IPOMEDT/main.py
+++ IPOMEDT/main.py
@@ -23,24 +23,34 @@ def main() -> None:
 
     # wacht 0.1 seconden voordat hij begint met het uitvoeren van script
     time.sleep(0.1)
-
-    while linefollower.poll():
-        # terwel de auto rijde op een zwarte stip voer de onderstande code uit:
+    # terwel de auto rijde op een zwarte stip voer de onderstande code uit:
+    while linefollower.poll() is True:
+        print("Auto rijd op zwart")
         testmotor1.forward()
         testmotor2.forward()
         # terwel de afstandsmeter een obstakel heeft gedectecteerd van 10 <  centimeter of kleiner
-        while ultrasonic.poll() < 11:  # kleiner dan 10 centimeter
-            # remmen stop de wilen met rijden
+        while ultrasonic.poll() < 11:
+            # Mischien is afremmen onodig
+            print("Afremen")
             testmotor1.stop()
             testmotor2.stop()
-            time.sleep(0.25)
-            testmotor1.forward()
-        else:
-            testmotor1.forward()
-            testmotor2.forward()
+            # de robot gaat naar links
+            print("draai 90 grade naar links")
+            testmotor1.forward(10)
+            testmotor2.backwards(20)
+            time.sleep(0.5)
+            links = 1
+            # Als de afstandmeter een obstakel heeft gedecteerd na het draien naar links
+            if links == 1 & ultrasonic.poll() < 11:
+                print("opstakel  gedecteerd")
+                print("draai 180 grade naar rechts")
+                testmotor2.forward(10)
+                testmotor1.backward(20)
+                time.sleep(1.0)
 
-    testmotor1.stop()
-    testmotor2.stop()
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    motor.stop()
 
 if __name__ == "__main__":
     main()
