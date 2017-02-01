e9a4ce7ac29691b4e3ebeb7f3d9c753f511c3490
Miesvanderlippe <miesvanderlippe@gmail.com>
2017-02-01 18:50:46 +0100

    Documentatie linefollower

--- IPOMEDT/classes/lineFollower.py
+++ IPOMEDT/classes/lineFollower.py
@@ -3,12 +3,21 @@ import time
 
 
 class LineFollower:
-
-    def __init__(self, pin):
+    def __init__(self, pin: int):
+        """
+        Constructor voor de linefollower (zwart / wit) sensor
+        :param pin: Pin waaraan de linefollower sensor zit
+        """
         self.pin = pin
         GPIO.setup(self.pin, GPIO.IN)
 
-    def poll(self):
+    def poll(self) -> bool:
+        """
+        Kijkt of de ondergrond zwart of wit is. De sensor geeft hier high (1)
+        of low (0) voor terug. Omdat de pin op GPIO.IN staat kan je de status
+        van de pin lezen.
+        :return: Bool zwart of wit.
+        """
         return GPIO.input(self.pin) == 0
 
 
@@ -19,6 +28,7 @@ def main() -> None:
 
     sensor = LineFollower(25)
 
+    # leest de sensor 100 keer uit met 1/10e seconde ertussen.
     for i in range(0, 100):
         print(sensor.poll())
         time.sleep(0.1)
