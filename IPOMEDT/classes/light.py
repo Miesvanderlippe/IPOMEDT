b9ef60c5448111786e6c7dae65de63d87d362d1c
Miesvanderlippe <miesvanderlippe@gmail.com>
2017-02-01 13:26:41 +0100

    Support for blue/red sirens

--- IPOMEDT/classes/light.py
+++ IPOMEDT/classes/light.py
@@ -19,17 +19,33 @@ def main():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
 
-    left_light = Light(21)
-    right_light = Light(20)
+    left_light = Light(26)
+    right_light = Light(21)
+    red_sirene = Light(20)
+    blue_sirene = Light(16)
+
+    left_light.turn_off()
+    right_light.turn_off()
+    red_sirene.turn_off()
+    blue_sirene.turn_off()
+
+    time.sleep(2)
 
     left_light.turn_on()
-    time.sleep(1)
-
+    time.sleep(0.5)
     right_light.turn_on()
-    time.sleep(1)
+    time.sleep(0.5)
+    red_sirene.turn_on()
+    time.sleep(0.5)
+    blue_sirene.turn_on()
+    time.sleep(0.5)
 
-    right_light.turn_off()
+    time.sleep(10)
+
     left_light.turn_off()
+    right_light.turn_off()
+    red_sirene.turn_off()
+    blue_sirene.turn_off()
 
 if __name__ == '__main__':
     main()
